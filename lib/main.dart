77be7196c924d743aaca6bdb19e841a36aad33d0
Jaiafavisuals <jaiafavisuals@gmail.com>
2023-05-19 00:10:22 +0400

    color setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,37 @@
 import 'package:flutter/material.dart';
 import 'package:expenses_app/widgets/expenses.dart';
 
-var kColorScheme =
-    ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 96, 59, 181));
+var kColorScheme = ColorScheme.fromSeed(
+  seedColor: const Color.fromARGB(255, 96, 59, 181),
+);
 
 void main() {
   runApp(MaterialApp(
     theme: ThemeData().copyWith(
-        useMaterial3: true,
-        colorScheme: kColorScheme,
-        appBarTheme: const AppBarTheme().copyWith(
-            backgroundColor: kColorScheme.onPrimaryContainer,
-            foregroundColor: kColorScheme.onPrimaryContainer)),
+      useMaterial3: false,
+      colorScheme: kColorScheme,
+      appBarTheme: const AppBarTheme().copyWith(
+        backgroundColor: kColorScheme.onPrimaryContainer,
+        foregroundColor: kColorScheme.primaryContainer,
+      ),
+      cardTheme: const CardTheme().copyWith(
+        color: kColorScheme.secondaryContainer,
+        margin: const EdgeInsets.symmetric(
+          horizontal: 16,
+          vertical: 8,
+        ),
+      ),
+      elevatedButtonTheme: ElevatedButtonThemeData(
+        style: ElevatedButton.styleFrom(
+            backgroundColor: kColorScheme.primaryContainer),
+      ),
+      textTheme: const TextTheme().copyWith(
+        titleLarge: TextStyle(
+          color: kColorScheme.onSecondaryContainer,
+          fontSize: 14,
+        ),
+      ),
+    ),
     debugShowCheckedModeBanner: false,
     home: const Expenses(),
   ));
--- lib/widgets/expenses_list/expenses_item.dart
+++ lib/widgets/expenses_list/expenses_item.dart
@@ -12,32 +12,33 @@ class ExpensesItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Card(
-      child: Column(
-        children: [
-          Text(expense.title),
-          const Padding(
-            padding: EdgeInsets.symmetric(
-              vertical: 10,
-              horizontal: 7,
+      child: Padding(
+          padding: EdgeInsets.symmetric(
+            vertical: 16,
+            horizontal: 20,
+          ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(expense.title, style: Theme.of(context).textTheme.titleLarge,),
+            const SizedBox(height: 4),
+            Row(
+              children: [
+                Text('\$${expense.amount.toStringAsFixed(2)}'),
+                const Spacer(),
+                Row(
+                  children: [
+                    Icon(categoryIcons[expense.category]),
+                    const SizedBox(
+                      width: 8,
+                    ),
+                    Text(expense.formattedDate),
+                  ],
+                ),
+              ],
             ),
-          ),
-          const SizedBox(height: 4),
-          Row(
-            children: [
-              Text('\$${expense.amount.toStringAsFixed(2)}'),
-              const Spacer(),
-              Row(
-                children: [
-                  Icon(categoryIcons[expense.category]),
-                  const SizedBox(
-                    width: 8,
-                  ),
-                  Text(expense.formattedDate),
-                ],
-              ),
-            ],
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
